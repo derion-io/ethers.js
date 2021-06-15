f30bc3c974b0f1c70252fa0c54e770f1fb34e0b8
Zergity <zergity@gmail.com>
2021-06-15 08:32:19 +0700

    support array of addresses for getLogs

--- packages/providers/src.ts/base-provider.ts
+++ packages/providers/src.ts/base-provider.ts
@@ -1397,7 +1397,11 @@ export class BaseProvider extends Provider implements EnsProvider {
         const result: any = { };
 
         if (filter.address != null) {
-            result.address = this._getAddress(filter.address);
+            if (Array.isArray(filter.address)) {
+                result.address = filter.address.map(this._getAddress.bind(this));
+            } else {
+                result.address = this._getAddress(filter.address);
+            }
         }
 
         ["blockHash", "topics"].forEach((key) => {
